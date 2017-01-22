ee6a53a920cf4c031d9bf5a38aa7e95835adaebb
Karol Ksionek <karlkar@tlen.pl>
2017-01-22 11:17:52 +0100

    Swipe refresh layout

--- app/src/main/java/pl/kksionek/smogogrod/view/StatusFragment.java
+++ app/src/main/java/pl/kksionek/smogogrod/view/StatusFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -11,11 +12,11 @@ import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -29,16 +30,14 @@ import rx.Emitter;
 import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
-import rx.functions.Func2;
 import rx.schedulers.Schedulers;
 
 public class StatusFragment extends Fragment {
 
     private static final String TAG = "StatusFragment";
 
-    RecyclerView mRecyclerView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerView mRecyclerView;
     private Subscription mSubscription;
     private StatusAdapter mStatusAdapter = null;
     private Gson mGson;
@@ -58,48 +57,70 @@ public class StatusFragment extends Fragment {
             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_status, null);
 
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.fragment_status_swipe_layout);
+
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        boolean redownload = false;
         if (mStatusAdapter == null) {
             Log.d(TAG, "onCreateView: ");
             mStatusAdapter = new StatusAdapter();
-            redownload = true;
-        }
-        mRecyclerView.setAdapter(mStatusAdapter);
+            mRecyclerView.setAdapter(mStatusAdapter);
+            mSwipeRefreshLayout.setRefreshing(true);
+            refreshData(true);
+        } else
+            mRecyclerView.setAdapter(mStatusAdapter);
 
-        if (redownload) {
-            mSubscription = Observable.concat(
-                    Observable.fromEmitter(emitter -> {
-                        for (Station station : getStationsFromCache()) {
-                            StationDetails stationDetails = getStationDetailsFromCache(station.getStationId());
-                            if (stationDetails != null)
-                                emitter.onNext(new Pair<>(station, stationDetails));
-                        }
-                        emitter.onCompleted();
-                    }, Emitter.BackpressureMode.DROP),
-                    Network.getStations(getActivity())
-                    .subscribeOn(Schedulers.io())
-//                    .doOnNext(this::save)
-                    .flatMapIterable(stations -> stations)
-                    .filter(this::stationNeedDetails)
-                    .flatMap(station -> Network.getStationDetails(getActivity(), station.getStationId())
-                            .map(stationDetails -> new Pair<>(station, stationDetails))
-                            .subscribeOn(Schedulers.io()))
-                    .doOnError(Throwable::printStackTrace)
-                    .doOnNext(this::save)
-                    .retryWhen(errors ->
-                            errors
-                                    .zipWith(
-                                            Observable.range(1, 3), (n, i) -> i)
-                                    .flatMap(
-                                            retryCount -> Observable.timer(5L * retryCount, TimeUnit.SECONDS))))
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                            pair -> mStatusAdapter.add(pair),
-                            Throwable::printStackTrace);
-        }
+        mSwipeRefreshLayout.setOnRefreshListener(() -> refreshData(false));
+
         return view;
+    }
+
+    private void refreshData(boolean useCache) {
+        if (mSubscription != null && !mSubscription.isUnsubscribed())
+            mSubscription.unsubscribe();
+        mSubscription = Observable.concat(
+                Observable.fromEmitter(emitter -> {
+                    if (!useCache) {
+                        emitter.onCompleted();
+                        return;
+                    }
+                    for (Station station : getStationsFromCache()) {
+                        StationDetails stationDetails = getStationDetailsFromCache(station.getStationId());
+                        if (stationDetails != null)
+                            emitter.onNext(new Pair<>(station, stationDetails));
+                    }
+                    emitter.onCompleted();
+                }, Emitter.BackpressureMode.DROP),
+                Network.getStations(getActivity())
+                        .subscribeOn(Schedulers.io())
+//                    .doOnNext(this::save)
+                        .flatMapIterable(stations -> stations)
+                        .filter(this::stationNeedDetails)
+                        .flatMap(station -> Network.getStationDetails(getActivity(), station.getStationId())
+                                .map(stationDetails -> new Pair<>(station, stationDetails))
+                                .subscribeOn(Schedulers.io()))
+                        .doOnError(Throwable::printStackTrace)
+                        .doOnNext(this::save)
+                        .retryWhen(errors ->
+                                errors
+                                        .zipWith(
+                                                Observable.range(1, 3), (n, i) -> i)
+                                        .flatMap(
+                                                retryCount -> Observable.timer(5L * retryCount, TimeUnit.SECONDS))))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        pair -> {
+                            mStatusAdapter.add(pair);
+                            mSwipeRefreshLayout.setRefreshing(false);
+                        },
+                        throwable -> {
+                            Toast.makeText(
+                                    getActivity(),
+                                    "Problem z połączeniem. Spróbuj ponownie później.",
+                                    Toast.LENGTH_SHORT).show();
+                            throwable.printStackTrace();
+                            mSwipeRefreshLayout.setRefreshing(false);
+                        });
     }
 
     private boolean stationNeedDetails(Station station) {
